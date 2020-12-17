88a317701aead2240a14be4c06eb588f53cb28dd
lowkeyway <lowkeyway@126.com>
2020-12-17 09:26:50 +0800

    Transfer VS2017 to VS2019

--- 02_add_two_numbers/addTwoNum/addTwoNum/AddTwoNumbers.c
+++ 02_add_two_numbers/addTwoNum/addTwoNum/AddTwoNumbers.c
@@ -2,44 +2,17 @@
 #include <stdlib.h>
 #include "LeetCodeBox.h"
 #include "LeetCodeList.h"
+#include <stddef.h>
 
 struct ListNode {
 	unsigned int val;
 	struct list_head list;
 };
 
-unsigned int listToInt(struct list_head *head)
+
+void addListTail(struct list_head* head, unsigned int val)
 {
-	unsigned int ret = 0;
-	//struct ListNode *pos = NULL;
-
-	//list_for_each_entry(pos, head, list)
-	//{
-	//	ret = ret * 10 + pos->val;
-	//}
-
-	//for (pos = __container_of(head->list.next, pos, list); head != pos->list.next; pos = container_of(pos->list.next, pos, list))
-	//{
-	//	ret = ret * 10 + pos->val;
-	//}
-
-	return ret;
-}
-
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
-struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
-	return NULL;
-}
-
-void addListTail(struct list_head *head, unsigned int val)
-{
-	struct ListNode *entry = NULL;
+	struct ListNode* entry = NULL;
 
 	entry = malloc(sizeof(struct ListNode));
 	entry->val = val;
@@ -49,17 +22,75 @@ void addListTail(struct list_head *head, unsigned int val)
 	list_add_tail(&(entry->list), head);
 }
 
-void dumpList(struct list_head *head)
+void dumpList(struct list_head* head)
 {
-	//struct ListNode *pos;
+	struct ListNode* pos = NULL;
 
-	//for()
+	list_for_each_entry(pos, head, list)
+	{
+		printf("%d -> ", pos->val);
+	}
 }
+
+unsigned int listToInt(struct list_head *head)
+{
+	unsigned int ret = 0;
+	struct ListNode *pos = NULL;
+
+	list_for_each_entry(pos, head, list)
+	{
+		ret = ret * 10 + pos->val;
+	}
+
+	return ret;
+}
+
+unsigned char intToList(unsigned int num, struct list_head* head)
+{
+	unsigned int index;
+	if (!num)
+		return FALSE;
+
+	for (; num != 0; num = num/10)
+	{
+		index = num % 10;
+		printf("Add index[%d]\n", index);
+		addListTail(head, index);
+	}
+
+	return TRUE;
+}
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct list_head* addTwoNumbers(struct list_head* l1, struct list_head* l2) {
+	LIST_HEAD(L3);
+	INIT_LIST_HEAD(&L3);
+
+	unsigned int int1, int2, int3;
+
+	int1 = listToInt(l1);
+	int2 = listToInt(l2);
+
+	int3 = int1 + int2;
+	printf("int3 = %d\n", int3);
+
+	intToList(int3, &L3);
+
+	dumpList(&L3);
+
+	return NULL;
+}
+
 
 int main(void)
 {
-	unsigned int int1, int2;
-	struct ListNode *pos;
+	//struct ListNode *pos;
 
 	LIST_HEAD(L1);
 	LIST_HEAD(L2);
@@ -74,14 +105,11 @@ int main(void)
 	addListTail(&L2, 6);
 	addListTail(&L2, 4);
 
-	int1 = listToInt(&L1);
-	int2 = listToInt(&L2);
+	//pos = container_of(L1.next->next, struct ListNode, list);
+	//pos = container_of(L1.next->next, typeof(*pos), list);
+	//printf("pos->val = %d\n", pos->val);
 
-	printf("L1 = %d, L2 = %d\n", int1, int2);
-
-	pos = container_of(L1.next->next, struct ListNode, list);
-	//pos = container_of(L1.next->next, (_typeof_(*pos)), list);
-	printf("pos->val = %d\n", pos->val);
+	addTwoNumbers(&L1, &L2);
 
 	system("pause");
 	return 0;
--- 02_add_two_numbers/addTwoNum/addTwoNum/LeetCodeBox.h
+++ 02_add_two_numbers/addTwoNum/addTwoNum/LeetCodeBox.h
@@ -2,7 +2,9 @@
 #define _LEETCODE_BOX_
 
 
-#define ARRAY_SIZE(X) sizeof(X) / sizeof(X[0])
+#define ARRAY_SIZE(X)	sizeof(X) / sizeof(X[0])
+#define TRUE			1
+#define FALSE			0
 
 #endif // !_LEETCODE_BOX_
 #pragma once
