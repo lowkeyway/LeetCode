9c55daeb90c832b9a94d35d807ae22a40f3a3b79
lowkeyway <lowkeyway@126.com>
2021-01-18 17:45:46 +0800

    Finish 31th

--- 31.next-permutation/next-permutation/next-permutation/next-permutation.c
+++ 31.next-permutation/next-permutation/next-permutation/next-permutation.c
@@ -34,12 +34,37 @@ void dumpArray(int* nums, int size)
     printf("]\n");
 }
 
+void nextPermutation(int* nums, int numsSize) {
+    uint i = 0;
+    int k = -1, l = -1;
+
+    for (i = 0; i < numsSize - 1; i++)
+    {
+        if (nums[i] < nums[i + 1]) k = i;
+    }
+    printf("k = %d\n", k);
+    if (-1 == k)
+    {
+        trunAroundArray(nums, numsSize);
+        return;
+    }
+
+    for (i = 0; i < numsSize; i++)
+    {
+        if (nums[i] > nums[k]) l = i;
+    }
+    printf("l = %d\n", l);
+
+    swipe(&nums[k], &nums[l]);
+    trunAroundArray(&nums[k+1], numsSize - k - 1);
+}
+
 int main(void)
 {
-    int nums[] = { 1, 2, 3, 4};
+    int nums[] = { 1, 2, 7, 4, 3, 1};
     int numsSize = ARRAY_SIZE(nums);
     dumpArray(nums, numsSize);
-    trunAroundArray(nums, numsSize);
+    nextPermutation(nums, numsSize);
     dumpArray(nums, numsSize);
     return true;
 }
